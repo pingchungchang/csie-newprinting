479412bb8e3baaf4ad2ad6af019a5c9ffd295ab1
pingchungchang <pingchungchang@gmail.com>
2026-05-30 20:20:15 +0800

    [postgres-ha] fixed etcd/entrypoint.sh

--- ubuntu/postgres-ha/etcd/entrypoint.sh
+++ ubuntu/postgres-ha/etcd/entrypoint.sh
@@ -5,16 +5,14 @@ else
   CLUSTER_STATE="new"
 fi
 
-unset ETCD_INITIAL_CLUSTER ETCD_CLIENT_PORT ETCD_PEER_PORT
-
 exec etcd \
   --name=${NODE_NAME} \
   --data-dir=/etcd-data \
-  --listen-client-urls=http://0.0.0.0:${ETCD_CLIENT_PORT} \
-  --advertise-client-urls=http://${NODE_IP}:${ETCD_CLIENT_PORT} \
-  --listen-peer-urls=http://0.0.0.0:${ETCD_PEER_PORT} \
-  --initial-advertise-peer-urls=http://${NODE_IP}:${ETCD_PEER_PORT} \
-  --initial-cluster=${ETCD_INITIAL_CLUSTER} \
+  --listen-client-urls=http://0.0.0.0:${CLIENT_PORT} \
+  --advertise-client-urls=http://${NODE_IP}:${CLIENT_PORT} \
+  --listen-peer-urls=http://0.0.0.0:${PEER_PORT} \
+  --initial-advertise-peer-urls=http://${NODE_IP}:${PEER_PORT} \
+  --initial-cluster=${INITIAL_CLUSTER} \
   --initial-cluster-token=pg-ha-etcd-cluster \
   --initial-cluster-state=${CLUSTER_STATE} \
   --enable-v2=true
